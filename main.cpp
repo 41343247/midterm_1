f254d497b4052a4eb7618386c4b618018c1b95fb
41343247 <41343247@nfu.edu.tw>
2025-11-10 16:07:15 +0800

    改 main

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
+    Widget w,w2;
+    w.move(200,200);
+    w2.move(1000,200);
     w.show();
+    w2.show();
     return a.exec();
 }
